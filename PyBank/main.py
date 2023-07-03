d2bd722e8e26f1c3572bfdeaa2755e87bc297b8a
Whitney Sabrina <w.sabrinabriggs@gmail.com>
2023-07-03 19:59:29 -0400

    printed Election Results header & got Total Votes

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,11 +16,12 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 
 #Set initial value of the row counter to zero
 rowcount = 0
+budget = []
 profits_losses_total = 0
 monthly_change = 0
 gip = 0
 gdp = 0
-#Set initial value of the profit/loss counter to zero
+
 
 #Open the csv file and read the csv
 with open(csvpath) as file:
@@ -28,6 +29,8 @@ with open(csvpath) as file:
     
     header=next(file)
     #print(header)
+
+
 
 ## The total number of months included in the dataset
 ## The net total amount of "Profit/Losses" over the entire period
@@ -39,25 +42,43 @@ with open(csvpath) as file:
 
     for row in budget:
 #total number of rows/months
-        rowcount = rowcount + 1 
+        rowcount = rowcount + 1
+        print(file)
 #total profits/losses
-        if (int(row[1])) >= 0 or (int(row[1])) <= 0: 
-            profits_losses_total = profits_losses_total + (int(row[1])) 
+        #if (int(row[1])) >= 0 or (int(row[1])) <= 0: 
+            #profits_losses_total = profits_losses_total + (int(row[1])) 
 #changes in profits/losses ****THIS IS YIELDING INCORRECT VALUES**** 
-        if (int(row[1])) > 0 or (int(row[1])) < 0:        
-            monthly_change = (int(row[1])) - (int(monthly_change))
+        #current_month = [int(row[1])]
+        #next_month = current_month 
+        #if current_month > 0 or current_month < 0:
+         #   monthly_change = current_month
+        #print(current_month) 
+        #revenue = 0
+        #revenue = [int(current_month[0])]
+        #print(revenue)
+        #print(int(current_month[0]))
+
+        
+        #     budget = current_month 
+        #monthly_change = next_month - current_month
+        # #print(current_month) 
+        
+            
+            #print((int(row[1])))
+            #monthly_change = (int(row[1])) - (int(monthly_change))
             #print(monthly_change)
 #the greatest increase in profits (date and amount) ****THIS IS YIELDING INCORRECT VALUES**** 
-        if (int(row[1])) > 0 and (int(row[1])) != 0:  
-            #gip = (int(row[1])) 
-            gip = (max(row))   
-#the greatest decrease in profits (date and amount) ****THIS IS YIELDING INCORRECT VALUES**** 
-        if (int(row[1])) < 0 and (int(row[1])) != 0:  
-            #gdp = (int(row[1])) 
-            gdp = (min(row))
-    print("Total Months:", rowcount)
-    print("Total: $",profits_losses_total)
-    # #print(monthly_change)
-    print("Average Change: $",round(monthly_change/rowcount))
-    print("Greatest Increase in profits: $" , row[0], (max(row))) 
-    print("Greatest Decrease in profits: $" , row[0], (min(row))) 
+#         if (int(row[1])) > 0 and (int(row[1])) != 0:  
+#             #gip = (int(row[1])) 
+#             gip = (max(row))   
+# #the greatest decrease in profits (date and amount) ****THIS IS YIELDING INCORRECT VALUES**** 
+#         if (int(row[1])) < 0 and (int(row[1])) != 0:  
+#             #gdp = (int(row[1])) 
+#             gdp = (min(row))
+#     print("Total Months:", rowcount)
+#     print("Total: $",profits_losses_total)
+#     # #print(monthly_change)
+#     print("Average Change: $",round(monthly_change/rowcount))
+#     print("Greatest Increase in profits: $" , row[0], (max(row))) 
+#     print("Greatest Decrease in profits: $" , row[0], (min(row))) 
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,47 @@
+#The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
+#Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+
+##The total number of votes cast
+
+##A complete list of candidates who received votes
+
+##The percentage of votes each candidate won
+
+##The total number of votes each candidate won
+
+##The winner of the election based on popular vote
+
+import os
+import csv
+
+#Set path for the csv file
+csvpath = os.path.join("Resources", "election_data.csv")
+
+#set up variables here
+count = 0
+candidate_list = []
+
+
+#Open the csv file and read the csv
+with open(csvpath) as pyroll_file:
+    election_csv = csv.reader(pyroll_file)
+
+    #header = next(pyroll_file)
+
+    for row in election_csv:
+        count += 1
+
+        if row[2] == (next(iter((row[2])))):
+            candidate_list.append(row[2]) != candidate_list
+        else: 
+            #row[2] != (next(iter((row[2])))):
+            candidate_list.append(row[2]) == candidate_list
+    
+
+print("Election Results")
+print("-----------------------------------------")
+print("Total Votes: ", count)
+#print(candidate_list)
+#print("Winner: ", )
+
 
